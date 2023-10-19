3c775c00f1a54df672a081bcf3396ccd9bfe571b
CristianAndrioli <147204027+CristianAndrioli@users.noreply.github.com>
2023-10-19 00:09:49 -0300

    Add files via upload

---
+++ Cadastros.py
@@ -0,0 +1,351 @@
+from tkinter import*
+from tkinter import Tk, StringVar, ttk
+from tkinter import messagebox
+from webbrowser import BackgroundBrowser
+from PIL import Image, ImageTk
+from tkcalendar import Calendar, DateEntry
+from datetime import date
+from tkinter import filedialog as fd
+from view import *
+
+#Cores
+co0 = "#2e2d2b" # Preta
+co1 = "#feffff" #branca
+co2 = "#4fa882" # verde
+co3 = "#38576b" # valor
+co4 = "#403d3d"  # letra
+co5 = "#e06636"  # profit
+co6 = "#038cfc"  # azul
+co7 = "#3fbfb9"  # verde
+co8 = "#263238"  # +verde
+co9 = "#e9edf5"  # +verde
+
+# Criando janela
+janela = Tk()
+janela.title('')
+janela.geometry('900x600')
+janela.configure(background=co9)
+janela.resizable(width=FALSE, height=FALSE)
+
+style = ttk.Style(janela)
+style.theme_use("clam")
+
+# Criando frames
+frameCima = Frame(janela, width=1043, height=50, bg=co1, relief=FLAT)
+frameCima.grid(row=0, column=0)
+
+frameMeio = Frame(janela, width=1043, height=303, bg=co1, pady=20, relief=FLAT)
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+frameBaixo = Frame(janela, width=1043, height=300, bg=co1, relief=FLAT)
+frameBaixo.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW)
+
+# Criando funções -------------------------------------------------------------
+global tree
+
+#função inserir
+def inserir():
+    global imagem, imagem_strings, l_imagem
+
+    nome = e_nome.get()
+    local = e_local.get()
+    descricao = e_descricao.get()
+    model = e_model.get()
+    data = e_cal.get()
+    valor = e_valor.get()
+    serie = e_serial.get()
+    imagem = imagem_strings
+
+    lista_inserir = [nome, local, descricao, model, data, valor, serie, imagem]
+
+    for i in lista_inserir:
+        if i=='':
+            messagebox.showerror('Erro','Preencha todos os campos')
+            return
+
+    inserir_form(lista_inserir)
+
+    messagebox.showinfo('Sucesso', 'Os dados foram inseridos com sucesso')
+
+    e_nome.delete(0,'end')
+    e_local.delete(0,'end')
+    e_descricao.delete(0,'end')
+    e_model.delete(0,'end')
+    e_cal.delete(0,'end')
+    e_valor.delete(0,'end')
+    e_serial.delete(0,'end')
+
+    #for Widget in frameMeio.winfo_children():
+    #    Widget.destroy()
+
+    mostrar()
+
+# Função atualizar 
+def atualizar():
+    global imagem, imagem_strings, l_imagem
+    try:
+        treev_dados = tree.focus()
+        treev_dicionario = tree.item(treev_dados)
+        treev_lista = treev_dicionario['values']
+
+        valor = treev_lista[0]
+
+        e_nome.delete(0,'end')
+        e_local.delete(0,'end')
+        e_descricao.delete(0,'end')
+        e_model.delete(0,'end')
+        e_cal.delete(0,'end')
+        e_valor.delete(0,'end')
+        e_serial.delete(0,'end')
+
+        id = int(treev_lista[0])
+        e_nome.insert(0,treev_lista[1])
+        e_local.insert(0,treev_lista[2])
+        e_descricao.insert(0,treev_lista[3])
+        e_model.insert(0,treev_lista[4])
+        e_cal.insert(0,treev_lista[5])
+        e_valor.insert(0,treev_lista[6])
+        e_serial.insert(0,treev_lista[7])
+        imagem_strings = treev_lista[8]
+
+        def update():
+            global imagem, imagem_strings, l_imagem
+
+            nome = e_nome.get()
+            local = e_local.get()
+            descricao = e_descricao.get()
+            model = e_model.get()
+            data = e_cal.get()
+            valor = e_valor.get()
+            serie = e_serial.get()
+            imagem = imagem_strings
+
+            if imagem =='':
+                imagem = e_serial.insert(0,treev_lista[7])
+
+            lista_atualizar = [nome, local, descricao, model, data, valor, serie, imagem, id]
+
+            for i in lista_atualizar:
+                if i=='':
+                    messagebox.showerror('Erro','Preencha todos os campos')
+                    return
+              
+            atualizar_(lista_atualizar)
+            messagebox.showinfo('Sucesso', 'Os dados foram atualizados com sucesso')
+
+            e_nome.delete(0,'end')
+            e_local.delete(0,'end')
+            e_descricao.delete(0,'end')
+            e_model.delete(0,'end')
+            e_cal.delete(0,'end')
+            e_valor.delete(0,'end')
+            e_serial.delete(0,'end')
+
+            b_confirmar.destroy()
+
+            mostrar()          
+
+        b_confirmar = Button(frameMeio, command=update, width=13, text='Confirmar'.upper(), overrelief=RIDGE, font=('Ivy 8 bold'), bg=co2, fg=co1)
+        b_confirmar.place(x=330, y=185)
+
+    except IndexError:
+        messagebox.showerror('Erro','Selecione um dos dados da tabela')
+
+# Função deletar 
+def deletar():
+    global imagem, imagem_strings, l_imagem
+    try:
+        treev_dados = tree.focus()
+        treev_dicionario = tree.item(treev_dados)
+        treev_lista = treev_dicionario['values']
+        valor = treev_lista[0]
+
+        deletar_form([valor])
+
+        messagebox.showinfo('Sucesso', 'Os dados foram excluidos com sucesso')
+
+        mostrar()  
+
+    except IndexError:
+        messagebox.showerror('Erro','Selecione um dos dados da tabela')
+
+# Função para escolher imagem
+def escolher_imagem():
+    global imagem, imagem_strings, l_imagem
+
+    imagem = fd.askopenfilename()
+    imagem_strings = imagem
+
+    # Abrindo imagem
+    imagem = Image.open(imagem)
+    imagem = imagem.resize((170,170))
+    imagem = ImageTk.PhotoImage(imagem)
+
+    l_imagem = Label(frameMeio, image=imagem, bg=co1, fg=co4)
+    l_imagem.place(x=700, y=10)
+
+# Função para ver o item
+def ver_imagem():
+    global imagem, imagem_strings, l_imagem
+
+    treev_dados = tree.focus()
+    treev_dicionario = tree.item(treev_dados)
+    treev_lista = treev_dicionario['values']
+
+    valor = [int(treev_lista[0])]
+
+    item = ver_item(valor)
+
+    imagem = item[0][8]
+
+ # Abrindo imagem
+    imagem = Image.open(imagem)
+    imagem = imagem.resize((170,170))
+    imagem = ImageTk.PhotoImage(imagem)
+
+    l_imagem = Label(frameMeio, image=imagem, bg=co1, fg=co4)
+    l_imagem.place(x=700, y=10)
+
+#trabalhando no frame cima ---------------------------------------------------------
+# Abrindo imagem
+app_img = Image.open('inventario.png')
+app_img = app_img.resize((45,45))
+app_img = ImageTk.PhotoImage(app_img)
+
+app_logo = Label(frameCima, image=app_img, text=' Inventário Doméstico', width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
+app_logo.place(x=0, y=0)
+
+#trabalhando no frame meio --------------------------------------------------------
+#criando entradas
+l_nome = Label(frameMeio, text='Nome', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_nome.place(x=10, y=10)
+e_nome = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_nome.place(x=140, y=11)
+
+l_local = Label(frameMeio, text='Fornecedor', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_local.place(x=10, y=40)
+e_local = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_local.place(x=140, y=41)
+
+l_descricao = Label(frameMeio, text='Descrição', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_descricao.place(x=10, y=70)
+e_descricao = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_descricao.place(x=140, y=71)
+
+l_model = Label(frameMeio, text='Marca', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_model.place(x=10, y=100)
+e_model = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_model.place(x=140, y=101)
+
+l_cal = Label(frameMeio, text='Data da compra', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_cal.place(x=10, y=130)
+e_cal = DateEntry(frameMeio, width=12, backgroud='darkblue', bordewidth=2, year=2023)
+e_cal.place(x=140, y=131)
+
+l_valor = Label(frameMeio, text='Valor da compra', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_valor.place(x=10, y=160)
+e_valor = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_valor.place(x=140, y=161)
+
+l_serial = Label(frameMeio, text='Cód. Barras', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_serial.place(x=10, y=190)
+e_serial = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_serial.place(x=140, y=191)
+
+#Botão carregar
+l_carregar = Label(frameMeio, text='Imagem do produto', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_carregar.place(x=10, y=220)
+b_carregar = Button(frameMeio,command=escolher_imagem, width=29, text='Carregar imagem'.upper(),compound=CENTER, anchor=CENTER, overrelief=RIDGE, font=('Ivy 8'), bg=co1, fg=co0)
+b_carregar.place(x=140, y=221)
+
+# botão inserir
+img_add = Image.open('add.png')
+img_add = img_add.resize((20,20))
+img_add = ImageTk.PhotoImage(img_add)
+b_inserir = Button(frameMeio, command=inserir, image=img_add, width=95, text='  Adicionar'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE, font=('Ivy 8'), bg=co1, fg=co0)
+b_inserir.place(x=330, y=10)
+
+# botão Atualizar
+img_update = Image.open('update.png')
+img_update = img_update.resize((20,20))
+img_update = ImageTk.PhotoImage(img_update)
+b_update = Button(frameMeio, command=atualizar, image=img_update, width=95, text='  Atualizar'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE, font=('Ivy 8'), bg=co1, fg=co0)
+b_update.place(x=330, y=50)
+
+# botão Deletar
+img_delete = Image.open('delete.png')
+img_delete = img_delete.resize((20,20))
+img_delete = ImageTk.PhotoImage(img_delete)
+b_delete = Button(frameMeio, command=deletar, image=img_delete, width=95, text='  Deletar'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE, font=('Ivy 8'), bg=co1, fg=co0)
+b_delete.place(x=330, y=90)
+
+# botão Ver Imagem
+img_item = Image.open('item.png')
+img_item = img_item.resize((20,20))
+img_item = ImageTk.PhotoImage(img_item)
+b_item = Button(frameMeio, command=ver_imagem, image=img_item, width=95, text='  Ver imagem'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE, font=('Ivy 8'), bg=co1, fg=co0)
+b_item.place(x=330, y=221)
+
+# Label com quantidade total e valores
+l_total = Label(frameMeio, text='' ,width=14, height=2, anchor=CENTER, font=('Ivy 17 bold'), bg=co7, fg=co1)
+l_total.place(x=450, y=17)
+l_total_ = Label(frameMeio, text='     Valor Total dos Produtos     ', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co7, fg=co1)
+l_total_.place(x=450, y=12)
+
+l_qtd = Label(frameMeio, text='', width=14, height=2, pady=7, anchor=CENTER, font=('Ivy 17 bold'), bg=co7, fg=co1)
+l_qtd.place(x=450, y=90)
+l_qtd_ = Label(frameMeio, text='   Quantidade total dos itens   ', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co7, fg=co1)
+l_qtd_.place(x=450, y=92)
+
+#criação da tabela
+def mostrar():
+    global tree
+
+    # creating a treeview with dual scrollbars
+    tabela_head = ['#Item','Nome',  'Fornecedor','Descrição', 'Marca/Modelo', 'Data da compra','Valor da compra', 'Cód. Barras']
+
+    lista_itens = ver_form()
+
+    tree = ttk.Treeview(frameBaixo, selectmode="extended",columns=tabela_head, show="headings")
+
+    # vertical scrollbar
+    vsb = ttk.Scrollbar(frameBaixo, orient="vertical", command=tree.yview)
+
+    # horizontal scrollbar
+    hsb = ttk.Scrollbar(frameBaixo, orient="horizontal", command=tree.xview)
+
+    tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+    tree.grid(column=0, row=0, sticky='nsew')
+    vsb.grid(column=1, row=0, sticky='ns')
+    hsb.grid(column=0, row=1, sticky='ew')
+    frameBaixo.grid_rowconfigure(0, weight=12)
+
+    hd=["center","center","center","center","center","center","center", 'center']
+    h=[40,150,100,160,130,100,100, 100]
+    n=0
+
+    for col in tabela_head:
+        tree.heading(col, text=col.title(), anchor=CENTER)
+        # adjust the column's width to the header string
+        tree.column(col, width=h[n],anchor=hd[n])
+        n+=1
+
+    # inserindo os itens dentro da tabela
+    for item in lista_itens:
+        tree.insert('', 'end', values=item)
+
+    quantidade = [8888, 88]
+
+    for iten in lista_itens:
+        quantidade.append(float(iten[6]))
+
+    Total_valor = sum(quantidade)
+    Total_itens = len(quantidade)
+
+    #l_total['text'] = 'R$ {:,.2f}'.format(Total_valor)
+    l_total['text'] = 'R$ :' + str(Total_valor)
+    l_qtd['text'] = Total_itens
+
+mostrar()
+
+janela.mainloop()
---
+++ view.py
@@ -0,0 +1,54 @@
+# importando sqlite
+import sqlite3 as lite
+
+#criando conexao
+con = lite.connect('dados.db')
+
+
+# inserir dados
+def inserir_form(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO inventario(nome, local, descricao, marca, data_da_compra, valor_da_compra, serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
+        cur.execute(query,i)
+
+# Atualizar dados
+def atualizar_(i):
+    with con:
+        cur = con.cursor()
+        query = "UPDATE inventario SET nome=?, local=?, descricao=?, marca=?, data_da_compra=?, valor_da_compra=?, serie=?, imagem=? WHERE id=?"
+        cur.execute(query,i)
+
+# deletar dados
+def deletar_form(i):
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM inventario WHERE id=?"
+        cur.execute(query, i)
+
+# ver dados
+def ver_form():
+    ver_dados = []
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventario"
+        cur.execute(query)
+
+        rows = cur.fetchall()
+        for row in rows:
+            ver_dados.append(row)   
+    return ver_dados
+
+# ver dados individual
+def ver_item(id):
+    ver_dados_individual = []
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventario WHERE id=?"
+        cur.execute(query,id)
+
+        rows = cur.fetchall()
+        for row in rows:
+            ver_dados_individual.append(row)   
+    
+    return ver_dados_individual
